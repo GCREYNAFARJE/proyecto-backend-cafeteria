c2ce30b4a53596b6686b4e77c76f647f3af35544
Giancarlo Reynafarje Alocén <gcreynafarje@gmail.com>
2024-09-19 16:06:01 -0500

    avance

--- cafeteria/cafeteria/urls.py
+++ cafeteria/cafeteria/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path("", include("products.urls")),
     path("admin/", admin.site.urls),
-    path("productos/", include("products.urls")),
     path("usuarios/", include("users.urls")),
     path("pedidos/", include("orders.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cafeteria/orders/views.py
+++ cafeteria/orders/views.py
@@ -2,6 +2,7 @@ from django.views.generic import DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
+
 from .models import Order
 from .forms import OrderProductForm
 
